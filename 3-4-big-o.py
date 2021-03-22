dd01fae44fc0408bd425a06bf32e2a4a705be887
KIMINJOONG <ijboym216@gmail.com>
2021-03-22 21:55:21 +0900

    big o example

---
+++ 3-4-big-o.py
@@ -0,0 +1,11 @@
+def sum_all(n):
+    total = 0
+    for num in range(1, n + 1):
+        total += num
+    return total
+
+def sum_all2(n):
+    return int(n * (n + 1) / 2)
+
+print(sum_all(100))
+print(sum_all2(100))

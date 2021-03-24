a90ae2ce7aef43bff3f87f7c661e09ca3a02d5c0
KIMINJOONG <ijboym216@gmail.com>
2021-03-24 21:13:15 +0900

    hash - 1

---
+++ 3-5-hash.py
@@ -0,0 +1,28 @@
+hash_table = list([0 for i in range(10)])
+print(hash_table)
+
+def hash_func(key):
+    return key % 5
+
+data1 = 'Andy'
+data2 = 'Dave'
+data3 = 'Trump'
+
+print(ord(data1[0]), ord(data2[0]), ord(data3[0]))
+print(ord(data1[0]), hash_func(ord(data1[0])))
+
+def storage_data(data, value):
+    key = ord(data[0])
+    hash_address = hash_func(key)
+    hash_table[hash_address] = value
+
+storage_data('Andy', '000000')
+storage_data('Dave', '111111')
+storage_data('Trump', '222222')
+
+def get_data(data):
+    key =ord(data[0])
+    hash_address = hash_func(key)
+    return hash_table[hash_address]
+
+print(get_data('Andy'))

54682ae31fe043075dda1e34633f647750c37bd3
KIMINJOONG <ijboym216@gmail.com>
2021-05-07 22:01:58 +0900

    merge sort

--- 6-9-merge-sort.py
+++ 6-9-merge-sort.py
@@ -1,3 +1,5 @@
+import random
+
 def split(data):
     medium = int(len(data) / 2)
     left = data[:medium]
@@ -14,3 +16,32 @@ def mergesplit(data):
     left = mergesplit(data[:medium])
     right = mergesplit(data[medium:])
     return merge(left, right)
+
+def merge(left, right):
+    merged = list()
+    left_point, right_point = 0, 0
+
+    # case1: left/ right 아직 남아 있을때
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    # case2: left 만 남아있을때
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+
+    # case3: right 만 남아있을때
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+    return merged
+
+
+data_list = random.sample(range(100), 10)
+print(data_list)
+print(mergesplit(data_list))
